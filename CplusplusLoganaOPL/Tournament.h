58f70ebc41afdace76c2ad51442f1a7aea676d1e
Arjun Bastola <abastola@ramapo.edu>
2017-10-18 22:18:23 -0400

    Update Tournament.h

--- CplusplusLoganaOPL/Tournament.h
+++ CplusplusLoganaOPL/Tournament.h
@@ -1,29 +1,72 @@
+/*
+     ************************************************************
+     Name:  Arjun Bastola
+     Project:  Longana C++
+     Class:  1/Tournament
+     Date:  October 18, 2017
+     ************************************************************
+*/
+
+//Detail explanation in cpp files for each header file
+
 #pragma once
 #include "stdafx.h"
 #include "Round.h"
 
 class Tournament {
-private:
-	int tournaMentScore;
-	int player1Score;
-	int player2Score;
-	int roundNumber;
-	int roundRemaining;
-	bool loadFile();
-	vector <Domino> computerHand;
-	vector <Domino> playerHand;
-	deque <Domino> layout;
-	vector <Domino> boneyard;
-	bool p = false;
-	int t = 0;
-	bool loadedFromFile = false;
-	bool loadedRoundFirstTime = false;
 
 public:
+	//constructor
 	Tournament();
+	
+	//start playing the game
 	void playGame();
+	
+	//load the file
+	bool loadFile();
+	
+	//print tournament details
 	void printTournamentDetails();
-
+	
+private:
+	//store tournament score
+	int tournaMentScore;
+	
+	//store player 1 score
+	int player1Score;
+	
+	//store player 2 score
+	int player2Score;
+	
+	//store round number
+	int roundNumber;
+	
+	//store round number reamaining
+	int roundRemaining;
+	
+	//store domino of computer hand
+	vector <Domino> computerHand;
+	
+	//store domino of player hand
+	vector <Domino> playerHand;
+	
+	//store domino of layout
+	deque <Domino> layout;
+	
+	//store domino of boneyard
+	vector <Domino> boneyard;
+	
+	//temp store if passed
+	bool p = false;
+	
+	// temp store tournament score
+	int t = 0;
+	
+	//store if load from file
+	bool loadedFromFile = false;
+	
+	//store if first round
+	bool loadedRoundFirstTime = false;
 
 
 };
