17ee72e4dfa851a2aa6700f5c3b4e59a3a305259
Arjun Bastola <abastola@ramapo.edu>
2017-10-18 21:58:29 -0400

    Update Stock.h

--- CplusplusLoganaOPL/Stock.h
+++ CplusplusLoganaOPL/Stock.h
@@ -1,20 +1,42 @@
+/*
+     ************************************************************
+     Name:  Arjun Bastola
+     Project:  Longana C++
+     Class:  7/Stock
+     Date:  October 18, 2017
+     ************************************************************
+*/
+
 #include <vector>
 #include "Domino.h"
 
 
 class Stock {
-private:
-	
+
 
 public:
+	//Detail explanation in cpp files for each header file
+	
+	//Constructor 
 	Stock();
-	std::vector<Domino> deck;
+	
+	//Shuffle the deck
 	void shuffleTheDeck();
+	
+	//Distribute the hands
 	std::vector<Domino> DistributeAHand();
+	
+	//Draw a domino
 	Domino getTopDominoFromDeck();
+	
+	//print the deck
 	void printDeck();
-
+	
+	//Get the deck
 	std::vector<Domino> getStock();
+private:
+	//Get deck
+	std::vector<Domino> deck;
 
 
 };
