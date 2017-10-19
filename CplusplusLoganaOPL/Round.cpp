e5eb1ca85a5b21536a035027ae19adad54ce4a89
Arjun Bastola <abastola@ramapo.edu>
2017-10-18 20:26:46 -0400

    Partial ROund DOne

--- CplusplusLoganaOPL/Round.cpp
+++ CplusplusLoganaOPL/Round.cpp
@@ -1,5 +1,31 @@
+/*
+     ************************************************************
+     Name:  Arjun Bastola
+     Project:  Longana C++
+     Class:  2/Round
+     Date:  October 18, 2017
+     ************************************************************
+*/
+
 #include "stdafx.h"
 #include "Round.h"
+
+/* ********************************************************************* 
+Function Name: 
+	ROund(Constructor)
+Purpose: 
+	To intialize the round class. 
+Parameters: 
+	N/A
+Return Value: 
+	N/A
+Local Variables: 
+	N/A
+Algorithm: 
+	Initialize paranmeters of round class.
+Assistance Received: 
+	None 
+********************************************************************* */
 
 Round::Round(int a) {
 	countPassed = 0;
@@ -10,56 +36,138 @@ Round::Round(int a) {
 	gameEnded = false;
 }
 
-int Round::getCountPassed() {
-	return countPassed;
-}
-
-bool Round::isStockEmpty() {
-	return (stock.getStock()).empty();
-}
+/* ********************************************************************* 
+Function Name: 
+	setHands
+Purpose: 
+	Gets 8 hands for each player
+	Finds the player with engine and sets engine to true.
+	Sets turn according to whoever has the engine.
+Parameters: 
+	N/A
+Return Value: 
+	N/A
+Local Variables: 
+	N/A
+Algorithm: 
+	Get hand.
+	FInd Player with the engine.
+	Set Turn
+Assistance Received: 
+	None 
+********************************************************************* */
 
 void Round::setHands() {
+	
+	//shuffle the deck
 	stock.shuffleTheDeck();
+	
+	//draw dominos for each player
 	player2.setHand(Hand(stock.DistributeAHand()));
 	player1.setHand(Hand(stock.DistributeAHand()));
+	
+	//check if player 1 has engine and set the tunr
 	if (player1.hand.findEngine(engine)) {
 		enginePresent = true;
 		turn = 1;
 	}
+	
+	//check if player 2 has engine set the tunr
 	if (player2.hand.findEngine(engine)) {
 		enginePresent = true;
 		turn = 2;
 	}
 }
 
+/* ********************************************************************* 
+Function Name: 
+	printDetails
+Purpose:
+	Print round details like player's hand, deck, board.
+Parameters: 
+	N/A
+Return Value: 
+	N/A
+Local Variables: 
+	N/A
+Algorithm: 
+	Print the details	
+Assistance Received: 
+	None 
+********************************************************************* */
+
 void Round::printDetails() {
+	
+	//print player hands
 	cout << "\nPlayer 1 Hand: ";
 	player1.printHand();
 	cout << "\nPlayer 2 Hand: ";
 	player2.printHand();
+	
+	//print the round
 	cout << "\nRound " << (7 - engine) << " Stock: ";
+	
+	//print the stock
 	stock.printDeck();
+	
+	//print the boneyard
 	layout.printBoard();
 }
 
+/* ********************************************************************* 
+Function Name: 
+	findTurn
+Purpose:
+	finds the turn and play the turn
+	return the winner
+Parameters: 
+	N/A
+Return Value: 
+	return the winner
+Local Variables: 
+	N/A
+Algorithm: 
+	Play the turns
+	Return the winner
+Assistance Received: 
+	None 
+********************************************************************* */
+
 int Round::findTurn() {
+	
+	//check if hands are empty and return the player number in case of empty
 	if (player1.isHandEmpty()) {
 		return 1;
 	}
 	if (player2.isHandEmpty()) {
 		return 2;
 	}
+	
+	//check for turn and play them
 	if (turn == 1) {
+		
+		//display whose turn
 		cout << "\nPlayer 1 turn." << endl;
+		
+		//play the turn
 		playTurn(&player1);
+		
+		//change the turn
 		turn = 2;
 	}
 	else {
+		
+		//display whose turn
 		cout << "\nPlayer 2 turn." << endl;
+		
+		//play the turn
 		playTurn(&player2);
+		
+		//chaneg the turn
 		turn = 1;
 	}
-	printDetails();
+	
+	//return -1 if roudn not complete
 	return -1;
 }
 
@@ -220,12 +328,24 @@ void Round::placeFirstDomino() {
 
 }
 
+//getter for player1 score
 int Round::getPlayer1Sum() {
 	return player1.getSumOfHands();
 }
 
+//getter for player2 score
 int Round::getPlayer2Sum() {
 	return player2.getSumOfHands();
+}
+
+//getter for number of times passed
+int Round::getCountPassed() {
+	return countPassed;
+}
+
+//getter for if a stock is empty
+bool Round::isStockEmpty() {
+	return (stock.getStock()).empty();
 }
 
 
