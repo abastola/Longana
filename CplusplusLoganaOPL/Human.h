7935b093dfbbed788d2841cb57ac68b48a05ffc4
Arjun Bastola <abastola@ramapo.edu>
2017-10-18 21:18:08 -0400

    Update Human.h

--- CplusplusLoganaOPL/Human.h
+++ CplusplusLoganaOPL/Human.h
@@ -1,3 +1,15 @@
+/*
+     ************************************************************
+     Name:  Arjun Bastola
+     Project:  Longana C++
+     Class:  5/Human
+     Date:  October 18, 2017
+     ************************************************************
+*/
+
+//Only Need stuffs from Player
+//No hiding required
+
 #pragma once
 #include "Player.h"
 class Human : public Player {
