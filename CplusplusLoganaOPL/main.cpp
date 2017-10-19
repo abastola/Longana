21ec34290a87bb84ce683adc447af50364dfa267
Arjun Bastola <abastola@ramapo.edu>
2017-10-18 22:20:09 -0400

    Update main.cpp

--- CplusplusLoganaOPL/main.cpp
+++ CplusplusLoganaOPL/main.cpp
@@ -1,14 +1,23 @@
+/*
+     ************************************************************
+     Name:  Arjun Bastola
+     Project:  Longana C++
+     Class: 0/main
+     Date:  October 18, 2017
+     ************************************************************
+*/
+
+
 #include "stdafx.h"
 #include "Tournament.h"
 #include <fstream>
 
-void lineBreak() {
-	cout << "\n";
-}
-
 
 int main() {
+	//object of tournament class
 	Tournament tournament;
+	
+	//start the tournament
 	tournament.playGame();
 
 	system("pause");
