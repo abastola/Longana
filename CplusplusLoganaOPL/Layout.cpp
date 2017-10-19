6cab5838bd301ef5cbcbf7d068bf3377c5e67a95
Arjun Bastola <abastola@ramapo.edu>
2017-10-18 21:47:37 -0400

    Update Layout.cpp

--- CplusplusLoganaOPL/Layout.cpp
+++ CplusplusLoganaOPL/Layout.cpp
@@ -1,25 +1,48 @@
 #include "Layout.h"
 
+/* ********************************************************************* 
+Function Name: 
+	Layout(Constructor)
+Purpose: 
+	To intialize layout
+Parameters: 
+	N/A
+Return Value: 
+	N/A
+Local Variables: 
+	N/A
+Algorithm: 
+	Does nothing
+Assistance Received: 
+	None 
+********************************************************************* */
+
 Layout::Layout() {
 
 }
 
+
+//getter for left side of the layout
 Domino Layout::getLeft() {
 	return boardDominos[0];
 }
 
+//getter for right side of the layout
 Domino Layout::getRight() {
 	return boardDominos[boardDominos.size()-1];
 }
 
+//insert to left
 void Layout::insertLeft(Domino a) {
 	boardDominos.push_front(a);
 }
 
+//insert to right
 void Layout::insertRight(Domino a) {
 	boardDominos.push_back(a);
 }
 
+//print the board
 void Layout::printBoard() {
 	cout << "\nBoard  Layout: ";
 	cout << "L - ";
