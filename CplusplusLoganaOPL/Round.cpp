de8a113408807160b3de20425a8d927362fb91de
Arjun Bastola <abastola@ramapo.edu>
2017-10-18 20:56:51 -0400

    Update Round.cpp

--- CplusplusLoganaOPL/Round.cpp
+++ CplusplusLoganaOPL/Round.cpp
@@ -123,7 +123,7 @@ Purpose:
 Parameters: 
 	N/A
 Return Value: 
-	return the winner
+	returns the winner or -1 if no winner yet
 Local Variables: 
 	N/A
 Algorithm: 
@@ -171,13 +171,44 @@ int Round::findTurn() {
 	return -1;
 }
 
+/* ********************************************************************* 
+Function Name: 
+	playTurn
+Purpose:
+	Play the players turn
+Parameters: 
+	Player *player : Pointer to the player object
+Return Value: 
+	N/A
+Local Variables: 
+	Domino temp: Temporary domino for testing purpose
+	int requiredSide: PIP of player's side
+	bool requiredSide: To store if right domino
+	int playingDominoIndex: Index of domino choose
+Algorithm: 
+	Play the turns
+	Get the input
+	Validiate the input
+	Check if can be placed
+	If yes place the input
+Assistance Received: 
+	None 
+********************************************************************* */
 void Round::playTurn(Player *player) {
 
-	//Try to put on original side
+	//temp domino
 	Domino temp;
+	
+	//get required side depending on the player
 	int requiredSide;
+	
+	//if player 1
 	if (turn == 1) {
+		
+		//get left
 		temp = layout.getLeft();
+		
+		//if fliiped get side2 or else side1
 		if (temp.Flipped()) {
 			requiredSide = temp.getSideTwo();
 		}
@@ -185,8 +216,12 @@ void Round::playTurn(Player *player) {
 			requiredSide = temp.getSideOne();
 		}
 	}
+	//if player 2
 	else {
+		//get right
 		temp = layout.getRight();
+		
+		//if fliiped get side2 or else side1
 		if (temp.Flipped()) {
 			requiredSide = temp.getSideOne();
 		}
@@ -195,40 +230,71 @@ void Round::playTurn(Player *player) {
 		}
 	}
 
+	//index of playing domino
 	int playingDominoIndex = -1;
+	
+	//bool to store if correct choice
 	bool requiredSideOne = true;
 
+	//loop until correct input
 	while (true) {
+		
+		//get index of playing domino
 		playingDominoIndex = player->getInput(requiredSide);
+		
+		//if less than 0, it is draw of pass
 		if (playingDominoIndex < 0) {
+			
+			//if -1 pass
 			if (playingDominoIndex == -1) {
+				
+				//check if empty stock and let user draw
 				if ((stock.getStock()).size() == 0) {
 					countPassed++;
 					cout << "\nPlayer" << turn << " passed because of empty boneyard." << endl;
 					return;
 				}
+				
+				//let draw if not empty
 				else {
+					
+					//add a domino instead
 					player->hand.addDomino(stock.getTopDominoFromDeck());
 				}
+				
+				//print the details
 				printDetails();
+				
+				
 				continue;
 			}
+			
+			//if -2, means pass
 			if (playingDominoIndex == -2) {
+				
+				//set pass to true
 				passed = true;
 				countPassed++;
 				cout << "\nPlayer " << turn << " Passed." << endl;
 				return;
 			}
 		}
+		
+		//get the playing domino
 		temp = player->hand.getDomino(playingDominoIndex);
 
 		//check if returned domino is double
-
 		if (temp.getSideOne() == temp.getSideTwo()) {
+			
+			//if human ask where the domino should be places
 			if (turn == 1) {
+				
+				//get the side
 				cout << "\nWhich side do you want to put the double? 1 for Left and 2 for right: ";
 				int inputSide = 0;
 				cin >> inputSide;
+				
+				//insert on left or right
 				if (inputSide == 1) {
 					layout.insertLeft(temp);
 				}
@@ -236,9 +302,13 @@ void Round::playTurn(Player *player) {
 					layout.insertRight(temp);
 				}
 			}
+			
+			//if computer insert on right
 			else {
 				layout.insertRight(temp);
 			}
+			
+			// set passed to zero since insert
 			countPassed = 0;
 
 			cout << "\nPlayer" << turn << " placed ";
@@ -247,7 +317,8 @@ void Round::playTurn(Player *player) {
 			player->hand.removeDomino(playingDominoIndex);
 			return;
 		}
-
+		
+		//validiate the inputs
 		if (temp.getSideOne() == requiredSide) {
 			requiredSideOne = true;
 			break;
@@ -256,10 +327,12 @@ void Round::playTurn(Player *player) {
 			requiredSideOne = false;
 			break;
 		}
-
+		
+		//if user selects invalid, let them know
 		cout << "Invalid Domino Selection. Select one with matching PIP." << endl;
 	}
 
+	//flip the sides if required
 	if (requiredSideOne) {
 		if (turn == 1) {
 			temp.setFlipped();
@@ -270,29 +343,69 @@ void Round::playTurn(Player *player) {
 			temp.setFlipped();
 		}
 	}
-
+	
+	//insert to the layout
 	if (turn == 1) {
 		layout.insertLeft(temp);
 	}
 	else {
 		layout.insertRight(temp);
 	}
+	
+	//reset the passed
 	countPassed = 0;
+	
+	//print the play
 	cout << "\nPlayer" << turn << " placed ";
 	temp.printDomino();
 	cout << endl;
+	
+	//remove domino from the hand
 	player->hand.removeDomino(playingDominoIndex);
 
 }
 
+/* ********************************************************************* 
+Function Name: 
+	placeFirstDomino
+Purpose:
+	Finds the engines and places it for the user
+	If no engine found, deal one each domino
+Parameters: 
+	N/A
+Return Value: 
+	N/A
+Local Variables: 
+	int temp: store index of the engine domino
+	
+Algorithm: 
+	Find engine in hand
+	If not found, draw tiles until found
+	Play the engine in appropriate side
+Assistance Received: 
+	None 
+********************************************************************* */
+
 void Round::placeFirstDomino() {
+	
+	//loop this until engine found
 	while (true) {
+		
+		//if engine is not print
 		if (!enginePresent) {
+			
+			//print not present
 			cout << "\n\nEngine Not Found. Drawing one domino from stock." << endl;
+			
+			//draw dominos
 			player1.hand.addDomino(stock.getTopDominoFromDeck());
 			player2.hand.addDomino(stock.getTopDominoFromDeck());
+			
+			//print after draw
 			printDetails();
 
+			//find if engine present now for each players and set engnePresent to true if found
+			//also set the turn to player who has engine
 			if (player1.hand.findEngine(engine)) {
 				enginePresent = true;
 				turn = 1;
@@ -302,28 +415,51 @@ void Round::placeFirstDomino() {
 				turn = 2;
 			}
 		}
+		
+		//if engine found, output so
 		else {
 			cout << "\n\nEngine Found. Starting the game." << endl;
 			break;
 		}
 	}
 
+	//display which player has engine and starts first
 	cout << "\nPlayer " << turn << " starts first." << endl;
+	
+	//print the engine to be placed
 	cout << "\nPlayer " << turn << " placed Engine: " << engine << "-" << engine << endl;
 
+	//if player 1
 	if (turn == 1) {
+		//find the index of engine
 		int temp = player1.hand.findDominoWithTwoSides(engine, engine);
+		
+		//insert the engine
 		layout.insertLeft(player1.hand.getDomino(temp));
+		
+		//delete from hand
 		player1.deleteDominoFromHand(temp);
+		
+		//change the turn
 		turn = 2;
 	}
+	
+	//if player 2
 	else {
+		//find the index of engine
 		int temp = player2.hand.findDominoWithTwoSides(engine, engine);
+		
+		//insert the engine
 		layout.insertLeft(player2.hand.getDomino(temp));
+		
+		//delete from hand
 		player2.deleteDominoFromHand(temp);
+		
+		//change the turn
 		turn = 1;
 	}
 
+	//print the detail
 	printDetails();
 
 }
