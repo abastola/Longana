ac5130d5b091a4a8d41c28bb96c1d0d68f5d9a7a
Arjun Bastola <abastola@ramapo.edu>
2017-10-18 21:54:32 -0400

    Update Layout.h

--- CplusplusLoganaOPL/Layout.h
+++ CplusplusLoganaOPL/Layout.h
@@ -1,19 +1,42 @@
+/*
+     ************************************************************
+     Name:  Arjun Bastola
+     Project:  Longana C++
+     Class:  7/Layout
+     Date:  October 18, 2017
+     ************************************************************
+*/
+
 #pragma once
 #include "stdafx.h"
 #include "Domino.h"
 #include <deque>
 
 class Layout {
-	private:
-		
-
+	
 	public:
+		//Detail explanation in cpp files for each header file
+	
+		//Constructor 1
 		Layout();
-		deque<Domino> boardDominos;
+	
+		//Get left domino
 		Domino getLeft();
+	
+		//get right domino
 		Domino getRight();
+	
+		//insert to left
 		void insertLeft(Domino a);
+	
+		//insert to right
 		void insertRight(Domino a);
+		
+		//print the board
 		void printBoard();
+	
+	private:
+		//store the layout dominos
+		deque<Domino> boardDominos;
 			
 };
