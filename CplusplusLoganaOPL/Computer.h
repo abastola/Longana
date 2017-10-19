cc626815b950c8f18898ad5c881ec0c8d9858069
Arjun Bastola <abastola@ramapo.edu>
2017-10-18 21:19:18 -0400

    Update Computer.h

--- CplusplusLoganaOPL/Computer.h
+++ CplusplusLoganaOPL/Computer.h
@@ -1,7 +1,18 @@
+/*
+     ************************************************************
+     Name:  Arjun Bastola
+     Project:  Longana C++
+     Class:  6/Computer
+     Date:  October 18, 2017
+     ************************************************************
+*/
+
 #pragma once
 #include "Player.h"
 
 class Computer : public Player {
 	public:
 		int getInput(int requiredSide);
+	private:
+	
 };
